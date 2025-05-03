76253bca3c81bc5ae87be6a88efc730f87ef3a59
Neo Gao En <neoge07.24@ichat.sp.edu.sg>
2025-05-03 18:28:39 +0800

    additional test cases for lab 3

--- Lab3.py
+++ Lab3.py
@@ -11,8 +11,15 @@ def bubble_sort(arr, sorting_order):
 
     # Get number of elements in the list
     n = len(arr_result)
-
-    if n < 10:
+    
+    for i in range(n-1):
+        if arr_result[i] != int():
+            return 2
+    
+    
+    if n == 0:
+        return 0
+    elif n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -33,6 +40,8 @@ def bubble_sort(arr, sorting_order):
                 else:
                     # Return an empty array
                     arr_result = []
+    elif n >= 10:
+        return 1
     else:
         arr_result = -1
 
@@ -51,6 +60,9 @@ def main():
     print("Sorted array in descending order: ")
     result = bubble_sort(arr, SORT_DESCENDING)
     print(result)
+
+
+    
 
 if __name__ == "__main__":
     main()
--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,28 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_10plus():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 50, 50, 50, 50]
+
+    result = Lab3.bubble_sort(input_arr, 1)
+
+    assert (result == 1)
+
+
+def test_bubble_sort_0():
+    result = []
+    input_arr = []
+
+    result = Lab3.bubble_sort(input_arr, 1)
+
+    assert (result == 0)
+
+def test_bubble_sort_nonint():
+    result = []
+    input_arr = [64, 34, "hello", 12, 22, 11, 90]
+
+    result = Lab3.bubble_sort(input_arr, 2)
+
+    assert (result == 2)
